593c046a7be000b993e307048d5d31a88d0d136c
Ptown2442 <portervaughn93@gmail.com>
2023-10-29 20:59:38 -0700

    Improved loading skeleton experience

--- app/issues/_components/IssueForm.tsx
+++ app/issues/_components/IssueForm.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { Button, Callout, TextField } from "@radix-ui/themes";
-import dynamic from "next/dynamic";
+import SimpleMDE from "react-simplemde-editor";
 import { useForm, Controller } from "react-hook-form";
 import axios from "axios";
 import "easymde/dist/easymde.min.css";
@@ -15,10 +15,6 @@ import Spinner from "@/app/Components/Spinner";
 import { Issue } from "@prisma/client";
 
 type IssueFormData = z.infer<typeof issueSchema>;
-
-const SimpleMDE = dynamic(() => import("react-simplemde-editor"), {
-  ssr: false,
-});
 
 const IssueForm = ({ issue }: { issue?: Issue }) => {
   const [error, setError] = useState("");
--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,4 +1,9 @@
-import IssueForm from "../_components/IssueForm";
+import dynamic from "next/dynamic";
+
+const IssueForm = dynamic(() => import("@/app/issues/_components/IssueForm"), {
+  ssr: false,
+  loading: () => <p>Loading...</p>,
+});
 
 const NewIssuePage = () => {
   return <IssueForm />;
