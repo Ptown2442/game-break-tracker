88fb9177cb9b0931a68660b57bb250f8aafbe219
Ptown2442 <portervaughn93@gmail.com>
2023-11-01 20:14:24 -0700

    Add a login skeleton

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import Link from "next/link";
+import { Skeleton } from "@/app/Components";
 import { usePathname } from "next/navigation";
 import React from "react";
 import { AiFillBug } from "react-icons/ai";
@@ -70,7 +71,7 @@ const NavLinks = () => {
 const AuthStatus = () => {
   const { status, data: session } = useSession();
 
-  if (status === "loading") return null;
+  if (status === "loading") return <Skeleton width="3rem" />;
 
   if (status === "unauthenticated")
     return (
